9893a114959c910f220795e7ecc2051fc6df1bc6
bhargavpipl <bhargav@pipl.team>
2022-11-11 01:23:25 -0500

    Upgraded and Compatible to Magento2.4.5

--- Model/ResourceModel/Log.php
+++ Model/ResourceModel/Log.php
@@ -36,6 +36,6 @@ class Log extends AbstractDb
      */
     protected function _construct()
     {
-        $this->_init('MpdExtensions_login_as_customer', 'log_id');
+        $this->_init('mpdextensions_login_as_customer', 'log_id');
     }
 }
